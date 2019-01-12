c38e5b7694e3e56321cadad1d96a76cccf1688e1
Michael Flores <mike@michaelflores.io>
2019-01-12 13:20:26 -0800

    feat: add home sections and scroll snapping

--- src/containers/home/home3.js
+++ src/containers/home/home3.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
-import "./home.scss";
+import "./home3.scss";
 
 class Home extends Component {
   render() {
     return (
-      <div className="home" id="home">
-        <h1>Home</h1>
+      <div className="home3" id="home3">
+        <h1>Home 3</h1>
       </div>
     );
   }
