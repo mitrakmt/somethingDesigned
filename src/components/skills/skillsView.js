b4d4da7d3b5c758c60a474c5a5234c28963e9994
Michael Flores <mike@michaelflores.io>
2019-01-12 15:55:02 -0800

    feat: initial skills section infrastructure

---
+++ src/components/skills/skillsView.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react'
+import listOfDescriptions from './descriptionsList'
+import listOfSkills from './skillsList'
+
+const aboutView = () => (
+  <Fragment>
+    <div className="skillsColumn-header">
+      <h2>Skills</h2>
+    </div>
+    <div className="skillsColumn-list">
+      {listOfSkills.map(skill => (
+        <span>{skill}</span>
+      ))}
+    </div>
+    <div className="skillsColumn-descriptions">
+      {listOfDescriptions.map(description => (
+        <span>{description}</span>
+      ))}
+    </div>
+  </Fragment>
+)
+
+export default aboutView
