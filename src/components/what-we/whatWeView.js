8358ad434de3ce6a16fe8ffff7cf439bba1b81ca
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-01-19 17:57:07 -0600

    feat: what we page content and some design (#5)
    * feat: what we page content and design
    
    * feat: add scss import for font settings
    
    * fix: skills styling

---
+++ src/components/what-we/whatWeView.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react'
+
+import './whatWeView.scss'
+
+const what = () => (
+  <Fragment>
+    <div className="whatWeView-verticalRule" />
+    <div className="whatWeView-header">
+      <h2>
+        What <span className="whatWeView-header-coloredText">We</span>
+      </h2>
+    </div>
+    <p className="whatWeView-description">
+      WE’RE DESIGNERS & COPYWRITERS WHO ALSO KNOW MARKETING STRATEGY.
+      <br />
+      <br />
+      OUR SPECIALTIES ARE CREATING STRATEGIC MARKETING MATERIALS, FROM LOGOS TO
+      PITCH DECKS TO WEBSITES, AND MORE.
+      <br />
+      <br />
+      WHETHER YOU HAVE A COMPLETE DRAFT, A ROUGH SKETCH (WE LOVE A GOOD DOODLE),
+      OR JUST A REALLY GREAT IDEA, WE CAN HELP YOU DESIGN IT OR WRITE IT, OR
+      BOTH.
+    </p>
+  </Fragment>
+)
+
+export default what
--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -4,18 +4,21 @@ import Home from 'containers/home/home'
 import Home2 from 'containers/home/home2'
 import Home3 from 'containers/home/home3'
 import AboutContainer from 'containers/about/aboutContainer'
+import WhatWeContainer from 'containers/what-we/whatWeContainer'
 import SkillsContainer from 'containers/skills/skillsContainer'
 
 import './app.scss'
+import '../../styles/fonts.scss'
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className="app" id="root">
         <Home />
         <Home2 />
         <Home3 />
         <AboutContainer />
+        <WhatWeContainer />
         <SkillsContainer />
       </div>
     )
