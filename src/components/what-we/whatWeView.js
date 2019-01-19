bcb5f7a74e27b658698f41c29ff3d525b3301d31
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-01-19 16:24:01 -0600

    whatWe container updates

--- src/components/what-we/whatWeView.js
+++ src/components/what-we/whatWeView.js
@@ -4,23 +4,22 @@ import './whatWeView.scss'
 
 const what = () => (
   <Fragment>
-    <div className="whatWeView">
-      <div className="whatWeView-header">
-        <h1 className="whatWeView-header-title">What We</h1>
-      </div>
-      <p className="whatWeView-text">
-        WE’RE DESIGNERS & COPYWRITERS WHO ALSO KNOW MARKETING STRATEGY.
-        <br />
-        <br />
-        OUR SPECIALTIES ARE CREATING STRATEGIC MARKETING MATERIALS, FROM LOGOS
-        TO PITCH DECKS TO WEBSITES, AND MORE.
-        <br />
-        <br />
-        WHETHER YOU HAVE A COMPLETE DRAFT, A ROUGH SKETCH (WE LOVE A GOOD
-        DOODLE), OR JUST A REALLY GREAT IDEA, WE CAN HELP YOU DESIGN IT OR WRITE
-        IT, OR BOTH.
-      </p>
+    <div className="whatWeView-header">
+      <div className="whatWeView-verticalRule" />
+      <h2>What We</h2>
     </div>
+    <p className="whatWeView-description">
+      WE’RE DESIGNERS & COPYWRITERS WHO ALSO KNOW MARKETING STRATEGY.
+      <br />
+      <br />
+      OUR SPECIALTIES ARE CREATING STRATEGIC MARKETING MATERIALS, FROM LOGOS TO
+      PITCH DECKS TO WEBSITES, AND MORE.
+      <br />
+      <br />
+      WHETHER YOU HAVE A COMPLETE DRAFT, A ROUGH SKETCH (WE LOVE A GOOD DOODLE),
+      OR JUST A REALLY GREAT IDEA, WE CAN HELP YOU DESIGN IT OR WRITE IT, OR
+      BOTH.
+    </p>
   </Fragment>
 )
 
