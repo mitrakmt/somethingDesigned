e66fd8b7a66ac122275e7bd6dfaa130d3163f1e7
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-01-19 20:12:11 -0600

    complete in their component (#7)

--- src/containers/what-we/whatWeContainer.js
+++ src/containers/what-we/whatWeContainer.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
-import WhatWeView from 'components/what-we/whatWeView'
+import PinkTheme from 'components/pink-theme/pinkTheme'
 import './whatWeContainer.scss'
 
 class WhatWeContainer extends Component {
+  description = `WE’RE DESIGNERS & COPYWRITERS WHO ALSO KNOW MARKETING STRATEGY.
+  <br /><br />
+  OUR SPECIALTIES ARE CREATING STRATEGIC MARKETING MATERIALS, FROM LOGOS TO
+  PITCH DECKS TO WEBSITES, AND MORE.
+  <br /><br />
+  WHETHER YOU HAVE A COMPLETE DRAFT, A ROUGH SKETCH (WE LOVE A GOOD DOODLE),
+  OR JUST A REALLY GREAT IDEA, WE CAN HELP YOU DESIGN IT OR WRITE IT, OR
+  BOTH.`
+  titleWordOne = 'What'
+  titleWordTwo = 'We'
   render() {
     return (
       <div className="whatWeContainer" id="whatWe">
-        <WhatWeView />
+        <PinkTheme
+          titleWordOne={this.titleWordOne}
+          titleWordTwo={this.titleWordTwo}
+          description={this.description}
+        />
       </div>
     )
   }
