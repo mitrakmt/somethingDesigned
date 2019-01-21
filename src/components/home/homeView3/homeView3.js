0b5d5363d5e31c43a0ad76008c26ce7e7b93245d
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-01-20 21:59:13 -0600

    feat/home: home page view and designs completed

---
+++ src/components/home/homeView3/homeView3.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+import Fist from 'images/SDFist.jpg'
+
+import './homeView3.scss'
+
+const HomeView3 = () => (
+  <Fragment>
+    <div className="homeView3">
+      <img src={Fist} className="homeView3-fist" />
+    </div>
+  </Fragment>
+)
+
+export default HomeView3
