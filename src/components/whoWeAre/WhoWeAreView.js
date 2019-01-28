e283862cd01b0e16af32be58ba5fed23551f9832
Michael Flores <mike@michaelflores.io>
2019-01-27 16:12:39 -0800

    WIP: feat: who we are

--- src/components/whoWeAre/WhoWeAreView.js
+++ src/components/whoWeAre/WhoWeAreView.js
@@ -7,22 +7,28 @@ import './whoWeAreView.scss'
 const WhoWeAreView = () => (
   <Fragment>
     <div className="whoWeAreView">
-      <h1 className="whoWeAreView-title">We Can Turn Something</h1>
-      <p className="whoWeAreView-subtitle">Great</p>
-      <p className="whoWeAreView-text">
-        The team at Something Designed is packed full of professional miracle
-        workers. Their artistic direction, creative input and thorough attention
-        to detail is invaluable. We came to them with a half-baked idea, and
-        they turned it into a fully baked creative concept!
-        <br />
-        The swiftness at which they work is only matched by their creative
-        expertise. They are a privilege to work with and invest themselves in
-        the process—adding pearls of wisdom and thoughtful ideas all along the
-        way. I would trust this team with my highest creative priorities. This
-        is one badass group of women, who happen to be badass designers.
-        <br />
-        Chip Herter, Director of Syncronization, BUTTER MUSIC AND SOUND.
-      </p>
+      <div className="whoWeAreView-titleContainer">
+        <h1 className="whoWeAreView-title">We Can Turn Something</h1>
+        <p className="whoWeAreView-subtitle">Great</p>
+      </div>
+      <div className="whoWeAreView-textContainer">
+        <p className="whoWeAreView-text">
+          The team at Something Designed is packed full of professional miracle
+          workers. Their artistic direction, creative input and thorough
+          attention to detail is invaluable. We came to them with a half-baked
+          idea, and they turned it into a fully baked creative concept!
+        </p>
+        <p className="whoWeAreView-text">
+          The swiftness at which they work is only matched by their creative
+          expertise. They are a privilege to work with and invest themselves in
+          the process—adding pearls of wisdom and thoughtful ideas all along the
+          way. I would trust this team with my highest creative priorities. This
+          is one badass group of women, who happen to be badass designers.
+        </p>
+        <p className="whoWeAreView-text">
+          Chip Herter, Director of Syncronization, BUTTER MUSIC AND SOUND.
+        </p>
+      </div>
     </div>
   </Fragment>
 )
