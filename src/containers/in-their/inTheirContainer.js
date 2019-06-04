f4ddb9e457082eed15ac6e30a5a7d90ecac8e1de
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-06-03 23:13:07 -0500

    fix: quotes page (#23)
    * fix/quotesPage: responsive changes and navigation changes
    
    * fix/quotesPage: complete quote changes

--- src/containers/in-their/inTheirContainer.js
+++ src/containers/in-their/inTheirContainer.js
@@ -13,24 +13,24 @@ class InTheirContainer extends Component {
     this.titleWordTwo = 'Their'
     this.quotes = [
       `<p class="pinkTheme-quotes">"</p>
-      <br /><br />
+      <br />
       Something Designed’s decks are the best in the business. Their impact on our initial concept and existing materials was exponential. Their work has resulted in a smoother sales process for our team. 
       <br /><br />
       They have a fresh perspective, creative ideas and a great eye - and  are extremely passionate about delivering a perfect final product. They are without a doubt an invaluable asset to any creative project, and I would highly recommend them.
       <br /><br />
       Daniel Sena, Head of Strategic Marketing, INTERSCOPE RECORDS`,
       `<p class="pinkTheme-quotes">"</p>
-      <br /><br />
+      <br />
       The Something Designed team did an incredible job taking my text-heavy powerpoint and making it a visually engaging and digestable deck that felt on-brand. My ideas really came to life after they waved their magic wand. Not to mention, they are the easiest and most lovely humans to work with. They are truly brand deck gurus.
       <br /><br />
       Holly Liss, Founder, ENLISST`,
       `<p class="pinkTheme-quotes">"</p>
-      <br /><br />
+      <br />
       Having Something Designed in our corner gave us the upper hand when developing our brand’s voice and vision, and when crafting new business pitch materials. They have over delivered on every project I’ve worked with them on.
       <br /><br />
       Ryan Choura, CEO & Founder, CHOURA EVENTS`,
       `<p class="pinkTheme-quotes">"</p>
-      <br /><br />
+      <br />
       When I needed a pitch deck for my media company, I reached out to Something Designed, who came highly recommended. After meeting them in person, I knew that these hip, forward-thinking millennials with tons of experience were perfect for my project. Not only did they help me craft a bold and powerful message in a visually appealing deck and logo, they were also patient during the revision process. They didn’t stop until I was completely satisfied with the final product. Now, every time I show my deck to friends or colleagues, they are in awe. Having such an informative and well-designed deck has given the confidence to pitch investors and ultimately realize my dream. Thank you, ladies. You definitely exceeded my expectations. 
       <br /><br />
       Elka Worner, Los Angeles Bureau Chief, FOX BUSINESS NETWORK`,
@@ -54,6 +54,12 @@ class InTheirContainer extends Component {
     clearInterval(this.rotateQuotes)
   }
 
+  selectQuote = index => {
+    this.setState({
+      selectedQuote: index,
+    })
+  }
+
   render() {
     return (
       <div className="inTheirContainer" id="inTheir">
@@ -63,6 +69,20 @@ class InTheirContainer extends Component {
           description={this.quotes[this.state.selectedQuote]}
           quotes
         />
+        <div className="inTheirContainer-quoteNavigation">
+          {this.quotes.map((quote, index) => (
+            <span
+              className={`inTheirContainer-dot ${
+                this.state.selectedQuote === index
+                  ? 'inTheirContainer-selected'
+                  : ''
+              }`}
+              onClick={() => {
+                this.selectQuote(index)
+              }}
+            />
+          ))}
+        </div>
       </div>
     )
   }
